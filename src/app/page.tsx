b97624fbc038b891216407cb8251109a9e2d8be6
kiya kebe <kiyakebe799@gmail.com>
2024-12-29 23:53:01 +0300

    chore: remove unwanted file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import { ScrollSpySidebar } from "@/components/Sidebar";
 import { ThemeToggle } from "@/components/ThemeToggler";
-import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Home() {
   return (
-    <SidebarProvider>
-      <div className="relative  w-full">
-        <ThemeToggle />
+    <div className="relative  w-full">
+      <ThemeToggle />
 
-        <ScrollSpySidebar />
-
-        <main>Kiya Kebe</main>
-      </div>
-    </SidebarProvider>
+      <main>Kiya Kebe</main>
+    </div>
   );
 }
