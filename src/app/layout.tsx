371da0cf739174453e1686ef26de41d153a58b91
kiya kebe <kiyakebe799@gmail.com>
2025-02-11 01:42:14 +0300

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Kiya Kebe | Web Developer & Designer",
+  title: "Kiya Kebe | Fullstack Web Developer & Designer",
   description: "I break down complex user experinece problems to create integritiy focussed solutions that connect you with your clients",
   icons: {
     icon: "/logoLight.png",
